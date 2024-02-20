ee25cf665e794abfe8f7e52a1f5684b40559b8fc
Yahya Fakhroji <yahya.fakhroji@gmail.com>
2024-02-20 23:49:09 +0700

    Add Sort Button, Move Search Bar, Improve Card List

--- src/app/(contacts)/(all)/page.tsx
+++ src/app/(contacts)/(all)/page.tsx
@@ -1,15 +1,26 @@
+'use client';
+
 import ContactCard from '@components/contact-card/contact-card.component';
+import IconSortAsc from '@components/icons/sort-asc';
+import SearchWidget from '@components/search/search.component';
+import Button from '@components/ui/button/button.component';
 
 import style from './style.module.scss';
 
 export default function All() {
-  const dummy = new Array(10).fill(0);
+  const dummy = new Array(20).fill(0);
 
   return (
-    <div className={style.container}>
-      {dummy.map((val, index) => {
-        return <ContactCard key={`contact_${index + 1}`} />;
-      })}
+    <div className={style.wrap}>
+      <div className={style.filter}>
+        <SearchWidget className={style.searchbar} />
+        <Button prefixIcon={<IconSortAsc />} className={style.sort} />
+      </div>
+      <div className={style.content}>
+        {dummy.map((val, index) => {
+          return <ContactCard key={`contact_${index + 1}`} />;
+        })}
+      </div>
     </div>
   );
 }
--- src/layouts/main/header.tsx
+++ src/layouts/main/header.tsx
@@ -1,5 +1,4 @@
 import IconAdd from '@components/icons/add';
-import SearchWidget from '@components/search/search.component';
 import Button from '@components/ui/button/button.component';
 
 import style from './style.module.scss';
@@ -8,7 +7,7 @@ export default function Header() {
   return (
     <header className={style.header}>
       <span className={style.title}>Contact List</span>
-      <SearchWidget className={style.searchbar} />
+      {/* <SearchWidget className={style.searchbar} /> */}
       <div className={style.actions}>
         <Button label="Add Contact" className={style.add} prefixIcon={<IconAdd />} />
       </div>
